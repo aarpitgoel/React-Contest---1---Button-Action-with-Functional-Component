85a57abf124b659fb8fcf97e6e45c202a534e7df
aarpitgoel <64373216+aarpitgoel@users.noreply.github.com>
2020-10-21 20:40:23 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [state, setState] = React.useState("");
   return (
     <div id="main">
-      // Do not alter the main div
+      <p id="para" >{state}</p>
+      <button id="button" onClick={() => setState("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")}></button>
     </div>
   );
 }
