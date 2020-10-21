31b514327e3e358b8ad4e25cf35441ad86ba15ef
aarpitgoel <64373216+aarpitgoel@users.noreply.github.com>
2020-10-21 20:43:47 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
   let [state, setState] = React.useState("");
   return (
     <div id="main">
-      <p id="para" >{state}</p>
-      <button id="button" onClick={() => setState("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")}></button>
+      <p id="para">{state}</p>
+      <button
+        id="click"
+        onClick={() =>
+          setState(
+            "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+          )
+        }
+      ></button>
     </div>
   );
 }
-
 
 export default App;
