3a63c1be74b8e41fb88e40b1490d8b5350a56f89
aarpitgoel <64373216+aarpitgoel@users.noreply.github.com>
2020-10-21 20:59:03 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,10 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  let [state, setState] = React.useState(false);
+  let [state, setstate] = React.useState(false);
   return (
     <div id="main">
-      <p id="para">{state}</p>
-      <button id="click" onClick={() => setState(true)}>
+      <button id="click" onClick={() => setstate(true)}>
         {state ? (
           <p id="para">
             Hello, I've learnt to use the full-stack evaluation tool. This makes
